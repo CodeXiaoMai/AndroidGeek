9fdf4b832e049babf225a73339231b3abd3642f8
wangce <949804781@qq.com>
2017-04-19 17:10:40 +0800

    修复部分6.0设备不能备份和导入的bug

--- app/src/main/java/com/xiaomai/geek/ui/module/password/FileExploreActivity.java
+++ app/src/main/java/com/xiaomai/geek/ui/module/password/FileExploreActivity.java
@@ -1,12 +1,17 @@
 
 package com.xiaomai.geek.ui.module.password;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -50,6 +55,7 @@ public class FileExploreActivity extends BaseActivity implements IFileExploreVie
     public static final String EXTRA_PATH = "extra_path";
 
     private static final String TAG = "FileExploreActivity";
+    private static final int REQUEST_CODE_FILE_PERMISSION = 0x0003;
 
     private int mCurrentType = TYPE_FILE;
 
@@ -83,6 +89,13 @@ public class FileExploreActivity extends BaseActivity implements IFileExploreVie
         ButterKnife.bind(this);
         initData();
         initViews();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, REQUEST_CODE_FILE_PERMISSION);
+        } else {
+            mPresenter.scanStorage(mContext);
+        }
     }
 
     private void initData() {
@@ -104,7 +117,6 @@ public class FileExploreActivity extends BaseActivity implements IFileExploreVie
                 .addItemDecoration(new HorizontalDividerItemDecoration.Builder(mContext).build());
 
         mPresenter.attachView(this);
-        mPresenter.scanStorage(mContext);
     }
 
     private void enterFolder(File file) {
@@ -243,5 +255,15 @@ public class FileExploreActivity extends BaseActivity implements IFileExploreVie
             }
         });
         recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mPresenter.scanStorage(mContext);
+        } else {
+            Snackbar.make(recyclerView, "您没有授予访问文件的权限，请重试！", Snackbar.LENGTH_LONG).show();
+        }
     }
 }

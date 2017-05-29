40cbb55e111d2be12e7dca3807b1fd02b65b5fcd
wangce <949804781@qq.com>
2017-05-29 18:56:34 +0800

    优化文章阅读体验

--- app/src/main/java/com/xiaomai/geek/ui/module/articel/ArticleDetailActivity.java
+++ app/src/main/java/com/xiaomai/geek/ui/module/articel/ArticleDetailActivity.java
@@ -2,9 +2,14 @@ package com.xiaomai.geek.ui.module.articel;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
+import android.view.View;
+import android.webkit.WebView;
 
-import com.xiaomai.geek.R;
 import com.xiaomai.geek.data.module.Article;
+import com.xiaomai.geek.data.pref.ArticlePref;
+import com.xiaomai.geek.ui.widget.MyWebView;
 
 /**
  * Created by XiaoMai on 2017/5/16.
@@ -13,6 +18,8 @@ import com.xiaomai.geek.data.module.Article;
 public class ArticleDetailActivity extends WebViewActivity {
 
     public static final String EXTRA_ARTICLE = "extra_article";
+    private int mReadProgress;
+    private String mArticleUrl;
 
     public static void launch(Context context, Article article) {
         Intent intent = new Intent(context, ArticleDetailActivity.class);
@@ -21,7 +28,7 @@ public class ArticleDetailActivity extends WebViewActivity {
     }
 
     @Override
-    protected void initViews() {
+    protected void loadData() {
         Intent intent = getIntent();
         Article article = intent.getParcelableExtra(EXTRA_ARTICLE);
         mTitle = article.getName();
@@ -29,28 +36,40 @@ public class ArticleDetailActivity extends WebViewActivity {
         setSupportActionBar(toolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mUrl = article.getUrl();
-        ArticleDetailFragment fragment = ArticleDetailFragment.newInstance(mUrl);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fl_container, fragment)
-                .commit();
+        mWebView.loadUrl(mUrl);
     }
 
     @Override
-    protected void loadData() {
+    protected void initCustomWebView(MyWebView webView) {
+        super.initCustomWebView(webView);
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                mReadProgress = scrollY;
+            }
+        });
+    }
+    @Override
+    protected void onLoadFinish(final WebView view, String url) {
+        super.onLoadFinish(view, url);
+        mArticleUrl = url;
+        mReadProgress = ArticlePref.getReadProgress(this, url);
+        final int progress = mReadProgress;
+        if (progress > 100) {
+            // 使用 SnackBar 根布局要使用 CoordinatorLayout
+            Snackbar.make(view, "是否跳转到上次阅读的位置", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("点击跳转", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            nestedScrollView.scrollTo(0, progress);
+                        }
+                    }).show();
+        }
     }
 
-    public void showToolBar(int duration) {
-//        if (toolBar.getTop() < top) {
-//            toolBar.setTop(toolBar.getTop() + duration);
-//            flContainer.setTop(flContainer.getTop() + duration);
-//        }
-    }
-
-    public void hideToolBar(int duration) {
-//        if (toolBar.getTop() > -top && toolBar.getTop() <= 0) {
-//            toolBar.setTop(toolBar.getTop() - duration);
-//            flContainer.setTop(flContainer.getTop() - duration);
-//        }
+    @Override
+    public void onPause() {
+        super.onPause();
+        ArticlePref.saveReadProgress(this, mArticleUrl, mReadProgress);
     }
 }
--- app/src/main/java/com/xiaomai/geek/ui/module/articel/WebViewActivity.java
+++ app/src/main/java/com/xiaomai/geek/ui/module/articel/WebViewActivity.java
@@ -1,19 +1,31 @@
 package com.xiaomai.geek.ui.module.articel;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import com.xiaomai.geek.R;
+import com.xiaomai.geek.common.utils.NetworkUtil;
 import com.xiaomai.geek.common.utils.ShareUtils;
 import com.xiaomai.geek.ui.base.BaseLoadActivity;
+import com.xiaomai.geek.ui.widget.MyWebView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,9 +44,14 @@ public class WebViewActivity extends BaseLoadActivity {
     FrameLayout flContainer;
     @BindView(R.id.coordinatorLayout)
     CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.nestedScrollView)
+    NestedScrollView nestedScrollView;
+
+    protected MyWebView mWebView;
 
     protected String mTitle;
-
     protected String mUrl;
 
     public static void launch(Context context, String url, String title) {
@@ -57,24 +74,117 @@ public class WebViewActivity extends BaseLoadActivity {
         Intent intent = getIntent();
         if (null == intent)
             return;
-        mUrl = intent.getStringExtra(EXTRA_URL);
-        if (TextUtils.isEmpty(mUrl))
-            return;
-        WebViewFragment fragment = WebViewFragment.newInstance(mUrl);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fl_container, fragment, String.valueOf(fragment.hashCode()))
-                .commit();
-    }
-
-    protected void initViews() {
-        Intent intent = getIntent();
-        if (intent == null)
-            return;
         mTitle = intent.getStringExtra(Intent.EXTRA_TITLE);
         toolBar.setTitle(mTitle);
         setSupportActionBar(toolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mUrl = intent.getStringExtra(EXTRA_URL);
+        if (TextUtils.isEmpty(mUrl))
+            return;
+        mWebView.loadUrl(mUrl);
+    }
+
+    protected void initViews() {
+        mWebView = new MyWebView(this);
+        flContainer.addView(mWebView);
+        initCustomWebView(mWebView);
+        initWebViewSettings();
+        initWebViewClient();
+    }
+
+    protected void initCustomWebView(MyWebView webView) {
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    protected void initWebViewSettings() {
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        if (NetworkUtil.isNetworkAvailable(this)) {
+            settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        } else {
+            settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+        // WebView中含有可以下载文件的链接，点击该链接后，应该开始执行下载的操作并保存文件到本地中。
+        // 重写 shouldOverrideUrlLoading 方法后总是 Crash
+//        mWebView.setDownloadListener(new DownloadListener() {
+//
+//            @Override
+//            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+//                Uri uri = Uri.parse(url);
+//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                startActivity(intent);
+//            }
+//        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            settings.setLoadsImagesAutomatically(true);
+        } else {
+            settings.setLoadsImagesAutomatically(false);
+        }
+    }
+
+    protected void initWebViewClient() {
+        WebViewClient webViewClient = new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /**
+                 * 当 MyWebView 将要加载一个新的 Url 之前，这个方法可以控制 MyWebView 的行为。
+                 * 这里就是拦截了 MyWebView 将要加载的 Url，即不让当前的 MyWebView 去加载，而是
+                 * 创建一个新的 Fragment 去加载这个 Url。
+                 *
+                 * 这样解决了WebView 回退时，刷新的问题
+                 */
+                WebViewActivity.launch(WebViewActivity.this, url, url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                onPageStart(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                onLoadFinish(view, url);
+            }
+        };
+        // 如果用户设置了WebViewClient，则在点击新的链接以后就不会跳转到系统浏览器了，而是在本WebView中显示。
+        mWebView.setWebViewClient(webViewClient);
+
+        WebChromeClient webChromeClient = new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress >= 100) {
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setProgress(newProgress);
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                setTitle(title);
+            }
+        };
+        mWebView.setWebChromeClient(webChromeClient);
+    }
+
+    protected void onPageStart(WebView view, String url, Bitmap favicon) {
+
+    }
+
+    protected void onLoadFinish(WebView view, String url) {
+        if (!mWebView.getSettings().getLoadsImagesAutomatically()) {
+            mWebView.getSettings().setLoadsImagesAutomatically(true);
+        }
     }
 
     @Override
@@ -96,5 +206,13 @@ public class WebViewActivity extends BaseLoadActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        flContainer.removeAllViews();
+        mWebView.removeAllViews();
+        mWebView.destroy();
     }
 }

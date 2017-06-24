eacdc77f35a9fd1d134dbeb376f17f948bb4f882
wangce <949804781@qq.com>
2017-06-24 15:18:43 +0800

    添加ijkplayer

---
+++ app/src/main/java/com/xiaomai/geek/ui/module/video/VideoDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.xiaomai.geek.ui.module.video
+
+import android.os.Bundle
+import com.xiaomai.geek.R
+import com.xiaomai.geek.ui.base.BaseActivity
+
+/**
+ * Created by XiaoMai on 2017/6/24.
+ */
+class VideoDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_detail)
+    }
+}

e8619a76c10f4ec298196de5bf2235e61f1002c5
xiaomai <949804781@qq.com>
2017-11-01 15:52:19 +0800

    恢复文章

--- app/src/main/java/com/xiaomai/geek/common/utils/WidgetUtils.java
+++ app/src/main/java/com/xiaomai/geek/common/utils/WidgetUtils.java
@@ -15,7 +15,12 @@ public class WidgetUtils {
         // 颜色数组对应的状态
         int[][] states = new int[7][];
         int i = 0;
+        // enable 并且 按下时的状态
         states[i++] = new int[]{android.R.attr.state_enabled, android.R.attr.state_pressed};
+        // enable 并且获得焦点时的状态
+        // 虽然和上面的状态用的是同一个颜色，但是不能这样写
+        // states[i++] = new int[]{android.R.attr.state_enabled, android.R.attr.state_focused, android.R.attr.state_pressed};
+        // 这样代表三种状态同时满足，才会响应
         states[i++] = new int[]{android.R.attr.state_enabled, android.R.attr.state_focused};
         states[i++] = new int[]{android.R.attr.state_enabled, android.R.attr.state_selected};
         states[i++] = new int[]{android.R.attr.state_enabled};
